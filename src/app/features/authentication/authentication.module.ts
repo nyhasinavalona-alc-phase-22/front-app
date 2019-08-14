3d2dda27b45dd562d6a052db2f97a9455a22b21f
Ny Hasinavalona Randriantsarafara <randriantsarafaranyhasinavalon@gmail.com>
2019-08-14 13:53:11 +0300

    [AUTHENTICATION] Create sign in form #master

--- src/app/features/authentication/authentication.module.ts
+++ src/app/features/authentication/authentication.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { AuthenticationRoutingModule } from './authentication-routing.module';
 import { SignInRootComponent } from './containers/sign-in-root/sign-in-root.component';
+import { SignInFormComponent } from './components/sign-in-form/sign-in-form.component';
 
 @NgModule({
-  declarations: [SignInRootComponent],
+  declarations: [SignInRootComponent, SignInFormComponent],
   imports: [
     CommonModule,
     AuthenticationRoutingModule
