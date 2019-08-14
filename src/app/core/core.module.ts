fbafac7ab8c734be2757621cb5f18dc81cf47cea
Ny Hasinavalona Randriantsarafara <randriantsarafaranyhasinavalon@gmail.com>
2019-08-14 17:32:11 +0300

    [CORE] Set app state and register authentication state #master

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,12 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AuthenticationModule } from '../features/authentication/authentication.module';
+import { StoreModule } from '@ngrx/store';
+import { reducers, metaReducers } from './store/reducers/app.reducers';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../../environments/environment';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    AuthenticationModule
+    AuthenticationModule,
+    StoreModule.forRoot(reducers, {
+      metaReducers,
+      runtimeChecks: {
+        strictStateImmutability: true,
+        strictActionImmutability: true
+      }
+    }),
+    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production })
   ]
 })
 export class CoreModule { }
