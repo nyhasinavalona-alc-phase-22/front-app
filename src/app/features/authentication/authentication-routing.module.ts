88727ed0b745349ee6afdafa6d174be67de76713
Ny Hasinavalona Randriantsarafara <randriantsarafaranyhasinavalon@gmail.com>
2019-08-14 13:50:51 +0300

    [AUTHENTICATION] Add sign in route #master

--- src/app/features/authentication/authentication-routing.module.ts
+++ src/app/features/authentication/authentication-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SignInRootComponent } from './containers/sign-in-root/sign-in-root.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    {
+        path: 'sign-in',
+        component: SignInRootComponent
+    },
+    {
+        path: '',
+        redirectTo: 'sign-in',
+        pathMatch: 'full'
+    }
+];
 
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forChild(routes)],
+    exports: [RouterModule]
 })
 export class AuthenticationRoutingModule { }
