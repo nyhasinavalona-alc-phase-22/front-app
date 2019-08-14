48fe25d14aa555a9eb5cb8d2dd428fcfc078e314
Ny Hasinavalona Randriantsarafara <randriantsarafaranyhasinavalon@gmail.com>
2019-08-14 22:38:28 +0300

    [AUTHENTICATION] Register authentication effects #master

--- src/app/features/authentication/authentication.module.ts
+++ src/app/features/authentication/authentication.module.ts
@@ -4,6 +4,9 @@ import { CommonModule } from '@angular/common';
 import { AuthenticationRoutingModule } from './authentication-routing.module';
 import { SignInRootComponent } from './containers/sign-in-root/sign-in-root.component';
 import { SharedModule } from '../../shared/shared.module';
+import { EffectsModule } from '@ngrx/effects';
+import { AuthenticationEffects } from './store/effects/authentication.effects';
+import { AuthenticationService } from './services/authentication.service';
 
 @NgModule({
     declarations: [
@@ -12,7 +15,11 @@ import { SharedModule } from '../../shared/shared.module';
     imports: [
         CommonModule,
         AuthenticationRoutingModule,
-        SharedModule
+        SharedModule,
+        EffectsModule.forRoot([AuthenticationEffects])
+    ],
+    providers: [
+        AuthenticationService
     ]
 })
 export class AuthenticationModule {
