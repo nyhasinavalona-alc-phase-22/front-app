fc2732df25453a6983d0e21d53c09d8a24662814
Ny Hasinavalona Randriantsarafara <randriantsarafaranyhasinavalon@gmail.com>
2019-08-14 20:54:34 +0300

    [SHARED] Import Http client module #master

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,25 +2,28 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AuthenticationModule } from '../features/authentication/authentication.module';
 import { StoreModule } from '@ngrx/store';
-import { reducers, metaReducers } from './store/reducers/app.reducers';
+import { metaReducers, reducers } from './store/reducers/app.reducers';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { environment } from '../../environments/environment';
 import { StoreRouterConnectingModule } from '@ngrx/router-store';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule,
-    AuthenticationModule,
-    StoreModule.forRoot(reducers, {
-      metaReducers,
-      runtimeChecks: {
-        strictStateImmutability: true,
-        strictActionImmutability: true
-      }
-    }),
-    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production }),
-    StoreRouterConnectingModule.forRoot()
-  ]
+    declarations: [],
+    imports: [
+        CommonModule,
+        AuthenticationModule,
+        HttpClientModule,
+        StoreModule.forRoot(reducers, {
+            metaReducers,
+            runtimeChecks: {
+                strictStateImmutability: true,
+                strictActionImmutability: true
+            }
+        }),
+        StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production }),
+        StoreRouterConnectingModule.forRoot()
+    ]
 })
-export class CoreModule { }
+export class CoreModule {
+}
