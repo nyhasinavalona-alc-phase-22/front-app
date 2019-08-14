b18990f2d382d56c58c93be3902956b331e93b1c
Ny Hasinavalona Randriantsarafara <randriantsarafaranyhasinavalon@gmail.com>
2019-08-14 15:52:12 +0300

    [AUTHENTICATION] Use dynamic question form #master

--- src/app/features/authentication/containers/sign-in-root/sign-in-root.component.ts
+++ src/app/features/authentication/containers/sign-in-root/sign-in-root.component.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { QuestionBase } from '../../../../shared/types/question-base.class';
+import { TextBoxQuestion } from '../../../../shared/types/text-box-question.class';
 
 @Component({
-  selector: 'app-sign-in-root',
-  templateUrl: './sign-in-root.component.html',
-  styleUrls: ['./sign-in-root.component.scss']
+    selector: 'app-sign-in-root',
+    templateUrl: './sign-in-root.component.html',
+    styleUrls: ['./sign-in-root.component.scss']
 })
 export class SignInRootComponent implements OnInit {
+    questions: QuestionBase<any>[];
 
-  constructor() { }
+    constructor() {
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+        this.questions = [
+            new TextBoxQuestion({
+                type: 'email',
+                key: 'email',
+                label: '',
+                required: true,
+                order: 1
+            }),
+            new TextBoxQuestion({
+                type: 'password',
+                key: 'password',
+                required: true,
+                order: 2
+            })
+        ];
+    }
 
+    onSave() {
+        /* */
+    }
 }
