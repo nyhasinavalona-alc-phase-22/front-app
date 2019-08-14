337b3841e8eb7dac43c252a31e41ca211ef000ad
Ny Hasinavalona Randriantsarafara <randriantsarafaranyhasinavalon@gmail.com>
2019-08-14 15:52:58 +0300

    [SHARED] Declare dynamic form question and question form component, question control service #master

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { QuestionControlService } from './services/question-control.service';
+import { QuestionFormComponent } from './components/question-form/question-form.component';
+import { DynamicFormQuestionComponent } from './components/dynamic-form-question/dynamic-form-question.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+    declarations: [QuestionFormComponent, DynamicFormQuestionComponent],
+    imports: [
+        CommonModule,
+        ReactiveFormsModule
+    ],
+    exports: [
+        QuestionFormComponent,
+        DynamicFormQuestionComponent
+    ],
+    providers: [
+        QuestionControlService
+    ]
 })
-export class SharedModule { }
+export class SharedModule {
+}
