f05e257597bf2dfc2321beba330e9347809cfc1f
Ny Hasinavalona Randriantsarafara <randriantsarafaranyhasinavalon@gmail.com>
2019-08-14 22:38:01 +0300

    [AUTHENTICATION] Refactor authentication action #master

--- src/app/features/authentication/store/actions/authentication.actions.ts
+++ src/app/features/authentication/store/actions/authentication.actions.ts
@@ -1,12 +1,6 @@
 import { createAction, props } from '@ngrx/store';
 import { HttpErrorResponse } from '@angular/common/http';
 
-export const enum AuthenticationActionsTypes {
-    SIGN_IN = '[AUTHENTICATION] Sign in',
-    SIGN_IN_FAIL = '[AUTHENTICATION] Sign in Fail',
-    SIGN_IN_SUCCESS = '[AUTHENTICATION] Sign in Success'
-}
-
-export const signIn = createAction(AuthenticationActionsTypes.SIGN_IN, props<{ userName: string, password: string }>());
-export const signInFail = createAction(AuthenticationActionsTypes.SIGN_IN_FAIL, props<{ error: HttpErrorResponse }>());
-export const signInSuccess = createAction(AuthenticationActionsTypes.SIGN_IN_SUCCESS, props<{ user: object }>());
+export const signIn = createAction('[Authentication] Sign in', props<{ userName: string, password: string }>());
+export const signInFail = createAction('[Authentication] Sign in fail', props<{ error: HttpErrorResponse }>());
+export const signInSuccess = createAction('[Authentication] Sign in success', props<{ user: object }>());
