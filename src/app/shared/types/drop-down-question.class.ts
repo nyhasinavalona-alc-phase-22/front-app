89cf4adbe7b7b9c9cb323e0a9b0e9ec87db5a104
Ny Hasinavalona Randriantsarafara <randriantsarafaranyhasinavalon@gmail.com>
2019-08-14 15:53:14 +0300

    [SHARED] Create question class #master

---
+++ src/app/shared/types/drop-down-question.class.ts
@@ -0,0 +1,12 @@
+import { QuestionBase } from './question-base.class';
+import { QuestionType } from '../constants/question-type.constant';
+
+export class DropDownQuestion extends QuestionBase<string> {
+    controlType = QuestionType.DROP_DOWN;
+    options: { key: string, value: string };
+
+    constructor(options: {} = {}) {
+        super(options);
+        this.options = options['options'] || [];
+    }
+}
