ba8e1bf79e35cd5614e5cdd5319f27115d15b89c
Ny Hasinavalona Randriantsarafara <randriantsarafaranyhasinavalon@gmail.com>
2019-08-16 23:33:30 +0300

    [CORE] Create router actions ##master

---
+++ src/app/core/store/actions/router.actions.ts
@@ -0,0 +1,5 @@
+import { createAction, props } from '@ngrx/store';
+
+export const go = createAction('[Router] Go', props<{ path: any, queryParams?: object, extras?: object }>());
+export const back = createAction('[Router] Back');
+export const forward = createAction('[Router] Forward');
