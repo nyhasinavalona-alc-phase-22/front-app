286cbf9a6d35221949689be3daa4641830c7dc6b
Ny Hasinavalona Randriantsarafara <randriantsarafaranyhasinavalon@gmail.com>
2019-08-16 15:51:00 +0300

    [CORE] Create home root component #master

---
+++ src/app/core/containers/home-root/home-root.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-home',
+    templateUrl: './home-root.component.html',
+    styleUrls: ['./home-root.component.scss']
+})
+export class HomeRootComponent {
+}
