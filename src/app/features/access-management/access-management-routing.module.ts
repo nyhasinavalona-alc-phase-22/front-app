d26b0f0b154e2b48c9e6b21ed93809368c296ab3
Ny Hasinavalona Randriantsarafara <randriantsarafaranyhasinavalon@gmail.com>
2019-08-16 18:05:27 +0300

    [ACCESS MANAGEMENT] Create role route #master

--- src/app/features/access-management/access-management-routing.module.ts
+++ src/app/features/access-management/access-management-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { UserRootComponent } from './containers/user-root/user-root.component';
+import { RoleRootComponent } from './containers/role-root/role-root.component';
 
 const routes: Routes = [
     {
         path: 'user',
         component: UserRootComponent
+    },
+    {
+        path: 'role',
+        component: RoleRootComponent
     },
     {
         path: '',
