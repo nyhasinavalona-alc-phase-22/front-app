60470c9c8f80591b7b8d430947eca20fbcb58da8
Ny Hasinavalona Randriantsarafara <randriantsarafaranyhasinavalon@gmail.com>
2019-08-16 16:10:11 +0300

    [ACCESS MANAGEMENT] Create user route #master

--- src/app/features/access-management/access-management-routing.module.ts
+++ src/app/features/access-management/access-management-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UserRootComponent } from './containers/user-root/user-root.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    {
+        path: 'user',
+        component: UserRootComponent
+    }
+];
 
 @NgModule({
     imports: [RouterModule.forChild(routes)],
