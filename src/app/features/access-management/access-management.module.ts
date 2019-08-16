fa93abf6dc90da46447c061de60a94527f19ef89
Ny Andrianina Mamy Razafintsialonina <nyandrianinamamy@gmail.com>
2019-08-16 15:54:26 +0300

    [FEATURES] Create access management module #master

---
+++ src/app/features/access-management/access-management.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AccessManagementRoutingModule } from './access-management-routing.module';
+
+@NgModule({
+    declarations: [],
+    imports: [
+        CommonModule,
+        AccessManagementRoutingModule
+    ]
+})
+export class AccessManagementModule {
+}
