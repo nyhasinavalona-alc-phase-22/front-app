551d088960bd8de4803482a102e3863fd200e0db
Ny Hasinavalona Randriantsarafara <randriantsarafaranyhasinavalon@gmail.com>
2019-08-16 12:05:48 +0300

    [AUTHENTICATION] Ident file #master

--- src/app/features/authentication/services/authentication.service.ts
+++ src/app/features/authentication/services/authentication.service.ts
@@ -3,14 +3,15 @@ import { HttpClient } from '@angular/common/http';
 import { environment } from '../../../../environments/environment';
 import { map } from 'rxjs/operators';
 import { ApiResponse } from '../../../shared/types/api-response.interface';
+import { AuthenticatonServiceInterface } from './authenticaton-service-interface';
 
 @Injectable()
-export class AuthenticationService {
+export class AuthenticationService implements AuthenticatonServiceInterface {
     constructor(private http: HttpClient) {
     }
 
     signIn(payload: { userName: string, password: string }) {
-        return this.http.post(`${environment.baseUrl}/auth`, payload).pipe(
+        return this.http.post(`${ environment.baseUrl }/auth`, payload).pipe(
             map((response: ApiResponse) => response.data)
         );
     }
