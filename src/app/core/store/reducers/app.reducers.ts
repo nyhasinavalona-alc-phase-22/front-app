1b61eb08610f74d1749bcffdafbbba7f9d85a54d
Ny Andrianina Mamy Razafintsialonina <nyandrianinamamy@gmail.com>
2019-08-16 22:01:09 +0300

    [CORE] Implement store rehydratation #master

--- src/app/core/store/reducers/app.reducers.ts
+++ src/app/core/store/reducers/app.reducers.ts
@@ -1,20 +1,27 @@
-import { ActionReducerMap, MetaReducer } from '@ngrx/store';
-import { environment } from '../../../../environments/environment';
+import { ActionReducer, ActionReducerMap, MetaReducer } from '@ngrx/store';
 import {
-  authenticationReducer,
-  AuthenticationState
+    authenticationReducer,
+    AuthenticationState
 } from '../../../features/authentication/store/reducers/authentication.reducers';
 import { routerReducer, RouterReducerState } from '@ngrx/router-store';
+import { localStorageSync } from 'ngrx-store-localstorage';
 
 export interface State {
-  authentication: AuthenticationState;
-  router: RouterReducerState<any>;
+    authentication: AuthenticationState;
+    router: RouterReducerState<any>;
 }
 
 export const reducers: ActionReducerMap<State> = {
-  authentication: authenticationReducer,
-  router: routerReducer
+    authentication: authenticationReducer,
+    router: routerReducer
 };
 
-export const metaReducers: MetaReducer<State>[] = !environment.production ? [] : [];
+export function localStorageSyncReducer(reducer: ActionReducer<any>): ActionReducer<any> {
+    return localStorageSync({
+        keys: ['authentication'],
+        rehydrate: true
+    })(reducer);
+}
+
+export const metaReducers: MetaReducer<State>[] = [localStorageSyncReducer];
 
