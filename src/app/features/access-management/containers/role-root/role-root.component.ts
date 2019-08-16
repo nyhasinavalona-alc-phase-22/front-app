31e5e5452454cc634fa6df7a8948b1ee5bfe0974
Ny Hasinavalona Randriantsarafara <randriantsarafaranyhasinavalon@gmail.com>
2019-08-16 18:04:18 +0300

    [ACCESS MANAGEMENT] Create role root component #master

---
+++ src/app/features/access-management/containers/role-root/role-root.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-role-root',
+    templateUrl: './role-root.component.html',
+    styleUrls: ['./role-root.component.scss']
+})
+export class RoleRootComponent implements OnInit {
+
+    constructor() {
+    }
+
+    ngOnInit() {
+    }
+
+}
