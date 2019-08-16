0b5079f74d033c0da4e361007ae27ead0d4043fc
Ny Andrianina Mamy Razafintsialonina <nyandrianinamamy@gmail.com>
2019-08-16 15:58:03 +0300

    [ACCESS MANAGEMENT] Reigster user root component #master

--- src/app/features/access-management/access-management.module.ts
+++ src/app/features/access-management/access-management.module.ts
@@ -2,9 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AccessManagementRoutingModule } from './access-management-routing.module';
+import { UserRootComponent } from './containers/user-root/user-root.component';
 
 @NgModule({
-    declarations: [],
+    declarations: [
+        UserRootComponent
+    ],
     imports: [
         CommonModule,
         AccessManagementRoutingModule
