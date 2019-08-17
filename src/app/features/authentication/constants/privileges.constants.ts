dbf0078a3ed1a5a19bbfb180f758aa6d9dabc7de
Ny Hasinavalona Randriantsarafara <randriantsarafaranyhasinavalon@gmail.com>
2019-08-17 20:29:57 +0300

    [CORE] Create privilege's constants ##master

---
+++ src/app/features/authentication/constants/privileges.constants.ts
@@ -0,0 +1,15 @@
+export const enum Privileges {
+    SAMPLE = '[Sample] Lorem ipsum'
+}
+
+export const PRIVILGE_LABELS: Record<Privileges, string> = {
+    [Privileges.SAMPLE]: 'Lorem ipsum'
+};
+
+export const enum PrivilegeCategories {
+    SAMPLE = 'Sample'
+}
+
+export const PRIVILEGE_CATEGORY_LABELS: Record<PrivilegeCategories, string> = {
+    [PrivilegeCategories.SAMPLE]: 'Sample'
+};
