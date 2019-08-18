58c1a08e5186406a1e3915792a00a3617fc62226
Ny Hasinavalona Randriantsarafara <randriantsarafaranyhasinavalon@gmail.com>
2019-08-18 15:47:03 +0300

    [ACCESS MANAGEMENT] Create user effects #master

---
+++ src/app/features/access-management/store/effects/user.effects.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { Actions, createEffect, ofType } from '@ngrx/effects';
+import { loadUsers, loadUsersFail, loadUsersSuccess } from '../actions/user.actions';
+import { catchError, map, switchMap, tap } from 'rxjs/operators';
+import { UserService } from '../../services/user.service';
+import { of } from 'rxjs';
+
+@Injectable()
+export class UserEffects {
+    loadUsers$ = createEffect(() => this.actions$.pipe(
+        ofType(loadUsers),
+        tap(() => console.log('load users')),
+        switchMap((action) => this.userService.loadUsers().pipe(
+            map((users) => loadUsersSuccess({ users })),
+            catchError((error) => of(loadUsersFail({ error })))
+        ))
+    ));
+
+    constructor(private actions$: Actions, private userService: UserService) {
+    }
+}
