7eaf64b8ee9191b4b812590d6fcf24b759c2cd7e
Ny Hasinavalona Randriantsarafara <randriantsarafaranyhasinavalon@gmail.com>
2019-08-18 15:46:54 +0300

    [ACCESS MANAGEMENT] Create user actions #master

---
+++ src/app/features/access-management/store/actions/user.actions.ts
@@ -0,0 +1,23 @@
+import { createAction, props } from '@ngrx/store';
+import { User } from '../../types/user.interface';
+import { HttpErrorResponse } from '@angular/common/http';
+
+export const loadUsers = createAction('[User] Load users');
+export const loadUsersFail = createAction('[User] Load users fail', props<{ error: HttpErrorResponse }>());
+export const loadUsersSuccess = createAction('[User] Load users success', props<{ users: User[] }>());
+
+export const loadUser = createAction('[User] Load user');
+export const loadUserFail = createAction('[User] Load user fail', props<{ error: HttpErrorResponse }>());
+export const loadUserSuccess = createAction('[User] Load user success', props<{ user: User }>());
+
+export const addUser = createAction('[User] Add user', props<{ user: User }>());
+export const addUserFail = createAction('[User] Add user fail', props<{ error: HttpErrorResponse }>());
+export const addUserSuccess = createAction('[User] Add user success', props<{ user: User }>());
+
+export const updateUser = createAction('[User] Update user', props<{ user: User }>());
+export const updateUserFail = createAction('[User] Update user fail', props<{ error: HttpErrorResponse }>());
+export const updateUserSuccess = createAction('[User] Update user success', props<{ user: User }>());
+
+export const deleteUser = createAction('[User] Delete user', props<{ user: User }>());
+export const deleteUserFail = createAction('[User] Delete user fail', props<{ error: HttpErrorResponse }>());
+export const deleteUserSuccess = createAction('[User] Delete user success', props<{ user: User }>());
