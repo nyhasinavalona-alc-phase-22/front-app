605a2ed51deb0b971f0d60c59c734f79fee1f3b3
Ny Hasinavalona Randriantsarafara <randriantsarafaranyhasinavalon@gmail.com>
2019-08-18 15:49:31 +0300

    [ACCESS MANAGEMENT] Register user's state #master

--- src/app/core/store/reducers/app.reducers.ts
+++ src/app/core/store/reducers/app.reducers.ts
@@ -5,14 +5,17 @@ import {
 } from '../../../features/authentication/store/reducers/authentication.reducers';
 import { routerReducer, RouterReducerState } from '@ngrx/router-store';
 import { localStorageSync } from 'ngrx-store-localstorage';
+import { userReducer, UserState } from '../../../features/access-management/store/reducers/user.reducers';
 
 export interface State {
     authentication: AuthenticationState;
+    user: UserState;
     router: RouterReducerState<any>;
 }
 
 export const reducers: ActionReducerMap<State> = {
     authentication: authenticationReducer,
+    user: userReducer,
     router: routerReducer
 };
 
