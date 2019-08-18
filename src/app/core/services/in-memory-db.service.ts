b3481f380e4ef60b2dd599bdbfba42bd388121d2
Ny Hasinavalona Randriantsarafara <randriantsarafaranyhasinavalon@gmail.com>
2019-08-18 16:01:51 +0300

    [CORE] Add one more user #master

--- src/app/core/services/in-memory-db.service.ts
+++ src/app/core/services/in-memory-db.service.ts
@@ -1,7 +1,11 @@
 import { Injectable } from '@angular/core';
 import { InMemoryDbService } from 'angular-in-memory-web-api';
 import { User } from '../../features/access-management/types/user.interface';
-import { PrivilegeCategories, Privileges } from '../../features/access-management/constants/privileges.constants';
+import {
+    PrivilegeCategories,
+    Privileges,
+    PRIVILGE_LABELS
+} from '../../features/access-management/constants/privileges.constants';
 
 @Injectable()
 export class InMemoryDBService implements InMemoryDbService {
@@ -19,8 +23,30 @@ export class InMemoryDBService implements InMemoryDbService {
                         {
                             id: 1,
                             name: Privileges.SAMPLE,
-                            label: 'Lorem ipsum',
+                            label: PRIVILGE_LABELS[Privileges.SAMPLE],
                             category: PrivilegeCategories.SAMPLE
+                        }
+                    ]
+                }
+            },
+            {
+                id: 2,
+                userName: 'user',
+                password: 'password',
+                email: 'user@mail.com',
+                fullName: 'first name last name',
+                firstName: 'first name',
+                lastName: 'last name',
+                role: {
+                    id: 1,
+                    label: 'Administrateur',
+                    name: 'administrator',
+                    privileges: [
+                        {
+                            id: 1,
+                            label: PRIVILGE_LABELS[Privileges.SAMPLE],
+                            category: PrivilegeCategories.SAMPLE,
+                            name: Privileges.SAMPLE
                         }
                     ]
                 }
