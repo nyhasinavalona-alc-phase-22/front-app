d48332865fb0cd65d14aba970208234479eae36a
Ny Hasinavalona Randriantsarafara <randriantsarafaranyhasinavalon@gmail.com>
2019-08-18 15:48:06 +0300

    [ACCESS MANAGEMENT] Register user's effect and services #master

--- src/app/features/access-management/access-management.module.ts
+++ src/app/features/access-management/access-management.module.ts
@@ -4,6 +4,12 @@ import { CommonModule } from '@angular/common';
 import { AccessManagementRoutingModule } from './access-management-routing.module';
 import { UserRootComponent } from './containers/user-root/user-root.component';
 import { RoleRootComponent } from './containers/role-root/role-root.component';
+import { EffectsModule } from '@ngrx/effects';
+import { UserRouterEffects } from './store/effects/user-router.effects';
+import { UserEffects } from './store/effects/user.effects';
+import { UserService } from './services/user.service';
+import { environment } from '../../../environments/environment';
+import { UserMockService } from './services/user-mock.service';
 
 @NgModule({
     declarations: [
@@ -12,7 +18,14 @@ import { RoleRootComponent } from './containers/role-root/role-root.component';
     ],
     imports: [
         CommonModule,
-        AccessManagementRoutingModule
+        AccessManagementRoutingModule,
+        EffectsModule.forFeature([UserEffects, UserRouterEffects])
+    ],
+    providers: [
+        {
+            provide: UserService,
+            useClass: environment.production ? UserService : UserMockService
+        }
     ]
 })
 export class AccessManagementModule {
