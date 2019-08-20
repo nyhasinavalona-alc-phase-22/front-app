c9dae9477eea4ca217a5b0e5f579c31d6161e00b
Ny Hasinavalona Randriantsarafara <randriantsarafaranyhasinavalon@gmail.com>
2019-08-20 14:32:09 +0300

    [SHARED] Register data list and cdk table module #table-component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,19 +4,24 @@ import { QuestionControlService } from './services/question-control.service';
 import { DynamicFormComponent } from './components/dynamic-form/dynamic-form.component';
 import { FormQuestionComponent } from './components/form-question/form-question.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { DataListComponent } from './components/data-list/data-list.component';
+import { CdkTableModule } from '@angular/cdk/table';
 
 @NgModule({
     declarations: [
         DynamicFormComponent,
-        FormQuestionComponent
+        FormQuestionComponent,
+        DataListComponent
     ],
     imports: [
         CommonModule,
-        ReactiveFormsModule
+        ReactiveFormsModule,
+        CdkTableModule
     ],
     exports: [
         DynamicFormComponent,
-        FormQuestionComponent
+        FormQuestionComponent,
+        DataListComponent
     ],
     providers: [
         QuestionControlService
