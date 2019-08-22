9ee74fa9f580e161cd2905e91155eb308b3b9828
Ny Hasinavalona Randriantsarafara <randriantsarafaranyhasinavalon@gmail.com>
2019-08-22 21:05:47 +0300

    [SHARED] Add sorting logic #table-component

--- src/app/shared/components/sortable-column/sortable-column.component.ts
+++ src/app/shared/components/sortable-column/sortable-column.component.ts
@@ -1,16 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, HostListener, Input, Output } from '@angular/core';
+import { Sort } from '../../types/sort.interface';
 
 @Component({
-    selector: 'app-sortable-column',
+    selector: 'app-sortable-column, [sortable]',
     templateUrl: './sortable-column.component.html',
     styleUrls: ['./sortable-column.component.scss']
 })
-export class SortableColumnComponent implements OnInit {
+export class SortableColumnComponent {
+    direction = 1;
+    @Input() sortable: string;
+    @Output() sort: EventEmitter<Sort> = new EventEmitter<Sort>();
 
-    constructor() {
+    @HostListener('click') onClick() {
+        this.sort.emit({ key: this.sortable, direction: this.direction });
+        this.direction = -this.direction;
     }
-
-    ngOnInit() {
-    }
-
 }
