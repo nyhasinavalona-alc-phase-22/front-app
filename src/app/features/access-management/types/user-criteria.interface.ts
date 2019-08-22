8652f2cadfe74b1470bfe0e8262f3f7d803da626
Ny Hasinavalona Randriantsarafara <randriantsarafaranyhasinavalon@gmail.com>
2019-08-22 21:06:56 +0300

    [SHARED] Add sort key #table-component

--- src/app/features/access-management/types/user-criteria.interface.ts
+++ src/app/features/access-management/types/user-criteria.interface.ts
@@ -1,5 +1,7 @@
 import { Paginator } from '../../../shared/types/paginator.interface';
+import { Sort } from '../../../shared/types/sort.interface';
 
 export interface UserCriteria {
     paginator: Paginator;
+    sort?: Sort;
 }
