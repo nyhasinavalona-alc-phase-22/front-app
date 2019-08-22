9d69695e207386a15df9caf6a62d29fe3cdf1ced
Ny Hasinavalona Randriantsarafara <randriantsarafaranyhasinavalon@gmail.com>
2019-08-22 21:07:11 +0300

    [SHARED] Add sort logic #table-component

--- src/app/features/access-management/services/user-mock.service.ts
+++ src/app/features/access-management/services/user-mock.service.ts
@@ -16,6 +16,17 @@ export class UserMockService extends UserServiceInterface {
 
     loadUsers(criteria: UserCriteria): Observable<Paginated<User>> {
         return this.http.get(`${ this.userUrl }`).pipe(
+            map((users: User[]) => {
+                if (criteria.sort) {
+                    return users.sort((a, b) => {
+                        if (typeof users[0][criteria.sort.key] === 'string') {
+                            return criteria.sort.direction * a[criteria.sort.key].localeCompare(b[criteria.sort.key]);
+                        }
+                        return criteria.sort.direction * ([criteria.sort.key] < b[criteria.sort.key] ? -1 : 1);
+                    });
+                }
+                return users;
+            }),
             map((users: User[]) => ({
                 items: paginate<User>(users, criteria.paginator),
                 totalItems: users.length
