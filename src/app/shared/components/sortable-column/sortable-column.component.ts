2b4042b78bc8da7a6747d28a061514b5628fc95d
Ny Hasinavalona Randriantsarafara <randriantsarafaranyhasinavalon@gmail.com>
2019-08-22 11:30:08 +0300

    [SHARED] Create sortable column component #table-component

---
+++ src/app/shared/components/sortable-column/sortable-column.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-sortable-column',
+    templateUrl: './sortable-column.component.html',
+    styleUrls: ['./sortable-column.component.scss']
+})
+export class SortableColumnComponent implements OnInit {
+
+    constructor() {
+    }
+
+    ngOnInit() {
+    }
+
+}
