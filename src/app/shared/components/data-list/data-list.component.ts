1126987973ef987ba83974e448c40da6e17d736a
Ny Hasinavalona Randriantsarafara <randriantsarafaranyhasinavalon@gmail.com>
2019-08-22 21:03:53 +0300

    [SHARED] Add sort logic #table-component

--- src/app/shared/components/data-list/data-list.component.ts
+++ src/app/shared/components/data-list/data-list.component.ts
@@ -1,5 +1,6 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { DataSource } from '@angular/cdk/table';
+import { Sort } from '../../types/sort.interface';
 
 @Component({
     selector: 'app-data-list',
@@ -10,4 +11,14 @@ export class DataListComponent<T> {
     @Input() items: T[];
     @Input() displayedColumns: string[];
     @Input() dataSource: DataSource<T>;
+    @Output() selection: EventEmitter<T> = new EventEmitter<T>();
+    @Output() sort: EventEmitter<Sort> = new EventEmitter<Sort>();
+
+    onClick(row: T) {
+        this.selection.emit(row);
+    }
+
+    onSort(sort: Sort) {
+        this.sort.emit(sort);
+    }
 }
