e01bc93b3c16a8448279cda23e8a3934e32b1a3b
Razafintsialonina Mamy <nyandrianinamamy@gmail.com>
2019-10-15 10:41:13 +0300

    [CORE] add injectable annotations to RouterEffects #master

--- src/app/core/store/effects/router.effects.ts
+++ src/app/core/store/effects/router.effects.ts
@@ -1,25 +1,47 @@
-import { Actions, createEffect, ofType } from '@ngrx/effects';
-import { back, forward, go } from '../actions/router.actions';
-import { tap } from 'rxjs/operators';
-import { Router } from '@angular/router';
 import { Location } from '@angular/common';
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { Actions, createEffect, ofType } from '@ngrx/effects';
+import { tap } from 'rxjs/operators';
+import { back, forward, go } from '../actions/router.actions';
 
+@Injectable()
 export class RouterEffects {
-    go$ = createEffect(() => this.actions$.pipe(
+  go$ = createEffect(
+    () =>
+      this.actions$.pipe(
         ofType(go),
-        tap((action) => this.router.navigate(action.path, { queryParams: action.queryParams, ...action.extras }))
-    ), { dispatch: false });
+        tap((action) =>
+          this.router.navigate(action.path, {
+            queryParams: action.queryParams,
+            ...action.extras,
+          }),
+        ),
+      ),
+    { dispatch: false },
+  );
 
-    back$ = createEffect(() => this.actions$.pipe(
+  back$ = createEffect(
+    () =>
+      this.actions$.pipe(
         ofType(back),
-        tap(() => this.location.back())
-    ), { dispatch: false });
+        tap(() => this.location.back()),
+      ),
+    { dispatch: false },
+  );
 
-    forward$ = createEffect(() => this.actions$.pipe(
+  forward$ = createEffect(
+    () =>
+      this.actions$.pipe(
         ofType(forward),
-        tap(() => this.location.forward())
-    ), { dispatch: false });
+        tap(() => this.location.forward()),
+      ),
+    { dispatch: false },
+  );
 
-    constructor(private actions$: Actions, private router: Router, private location: Location) {
-    }
+  constructor(
+    private actions$: Actions,
+    private router: Router,
+    private location: Location,
+  ) {}
 }
