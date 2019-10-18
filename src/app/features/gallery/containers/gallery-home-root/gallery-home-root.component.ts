d60266dc708405975e815e6c8d00c56f15065b6d
Ny Hasinavalona Randriantsarafara <randriantsarafaranyhasinavalon@gmail.com>
2019-10-18 16:05:20 +0300

    [GALLERY] Generate gallery home root #master

---
+++ src/app/features/gallery/containers/gallery-home-root/gallery-home-root.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-gallery-home-root',
+  templateUrl: './gallery-home-root.component.html',
+  styleUrls: ['./gallery-home-root.component.scss']
+})
+export class GalleryHomeRootComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/features/gallery/gallery.module.ts
+++ src/app/features/gallery/gallery.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { GalleryRoutingModule } from './gallery-routing.module';
+import { GalleryHomeRootComponent } from './containers/gallery-home-root/gallery-home-root.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [GalleryHomeRootComponent],
   imports: [
     CommonModule,
     GalleryRoutingModule
