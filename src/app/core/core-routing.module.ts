1bdabc4628bf4cc5bc68ec86e5e851f4503949b4
Ny Hasinavalona Randriantsarafara <randriantsarafaranyhasinavalon@gmail.com>
2019-10-18 16:09:05 +0300

    [CORE] Change default route #master

--- src/app/core/core-routing.module.ts
+++ src/app/core/core-routing.module.ts
@@ -4,28 +4,32 @@ import { HomeRootComponent } from './containers/home-root/home-root.component';
 import { SessionGuard } from './guards/session.guard';
 
 const routes: Routes = [
-    {
-        path: 'home',
-        component: HomeRootComponent,
-        canActivate: [SessionGuard],
-        canActivateChild: [SessionGuard],
-        children: [
-            {
-                path: 'access-management',
-                loadChildren: '../features/access-management/access-management.module#AccessManagementModule'
-            },
-            {
-                path: '',
-                redirectTo: 'access-management',
-                pathMatch: 'full'
-            }
-        ]
-    }
+  {
+    path: 'home',
+    component: HomeRootComponent,
+    canActivate: [SessionGuard],
+    canActivateChild: [SessionGuard],
+    children: [
+      {
+        path: 'gallery',
+        loadChildren: '../features/gallery/gallery.module#GalleryModule',
+      },
+      {
+        path: 'access-management',
+        loadChildren:
+          '../features/access-management/access-management.module#AccessManagementModule',
+      },
+      {
+        path: '',
+        redirectTo: 'gallery',
+        pathMatch: 'full',
+      },
+    ],
+  },
 ];
 
 @NgModule({
-    imports: [RouterModule.forRoot(routes)],
-    exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
 })
-export class CoreRoutingModule {
-}
+export class CoreRoutingModule {}
