f0f1057135ebe70216d97f0274e52cb472e18116
Ny Hasinavalona Randriantsarafara <randriantsarafaranyhasinavalon@gmail.com>
2019-10-18 17:02:37 +0300

    [GALLERY] Create video details root #master

---
+++ src/app/features/gallery/containers/video-details-root/video-details-root.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-video-details-root',
+  templateUrl: './video-details-root.component.html',
+  styleUrls: ['./video-details-root.component.scss']
+})
+export class VideoDetailsRootComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/features/gallery/gallery.module.ts
+++ src/app/features/gallery/gallery.module.ts
@@ -5,10 +5,11 @@ import { GalleryRoutingModule } from './gallery-routing.module';
 import { GalleryHomeRootComponent } from './containers/gallery-home-root/gallery-home-root.component';
 import { VideoListComponent } from './components/video-list/video-list.component';
 import { VideoCardComponent } from './components/video-card/video-card.component';
+import { VideoDetailsRootComponent } from './containers/video-details-root/video-details-root.component';
 
 
 @NgModule({
-  declarations: [GalleryHomeRootComponent, VideoListComponent, VideoCardComponent],
+  declarations: [GalleryHomeRootComponent, VideoListComponent, VideoCardComponent, VideoDetailsRootComponent],
   imports: [
     CommonModule,
     GalleryRoutingModule
