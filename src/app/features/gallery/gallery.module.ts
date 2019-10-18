efebbc91237faa6c883596adb226b026b1fbc38a
Ny Hasinavalona Randriantsarafara <randriantsarafaranyhasinavalon@gmail.com>
2019-10-18 16:39:20 +0300

    [GALLERY] Generate video card component #master

--- src/app/features/gallery/gallery.module.ts
+++ src/app/features/gallery/gallery.module.ts
@@ -4,10 +4,11 @@ import { CommonModule } from '@angular/common';
 import { GalleryRoutingModule } from './gallery-routing.module';
 import { GalleryHomeRootComponent } from './containers/gallery-home-root/gallery-home-root.component';
 import { VideoListComponent } from './components/video-list/video-list.component';
+import { VideoCardComponent } from './components/video-card/video-card.component';
 
 
 @NgModule({
-  declarations: [GalleryHomeRootComponent, VideoListComponent],
+  declarations: [GalleryHomeRootComponent, VideoListComponent, VideoCardComponent],
   imports: [
     CommonModule,
     GalleryRoutingModule
