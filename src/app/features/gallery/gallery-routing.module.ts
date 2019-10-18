186141e24821f12f153c2716a6445f337a3aec48
Ny Hasinavalona Randriantsarafara <randriantsarafaranyhasinavalon@gmail.com>
2019-10-18 17:04:53 +0300

    [GALLERY]  Create video details route #master

--- src/app/features/gallery/gallery-routing.module.ts
+++ src/app/features/gallery/gallery-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { GalleryHomeRootComponent } from './containers/gallery-home-root/gallery-home-root.component';
+import { VideoDetailsRootComponent } from './containers/video-details-root/video-details-root.component';
 
 const routes: Routes = [
   {
     path: '',
     component: GalleryHomeRootComponent,
+  },
+  {
+    path: 'details/:videoId',
+    component: VideoDetailsRootComponent,
   },
 ];
 
