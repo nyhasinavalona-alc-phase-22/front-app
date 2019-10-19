7d55bb42aa7c09068c30740bd8c53991e53fd3de
Ny Hasinavalona Randriantsarafara <randriantsarafaranyhasinavalon@gmail.com>
2019-10-19 20:11:57 +0300

    [GALLERY] Create video service #master

---
+++ src/app/features/gallery/services/video.service.ts
@@ -0,0 +1,14 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment.prod';
+import { Video } from '../types/video.interface';
+
+@Injectable()
+export class VideoService {
+  constructor(private http: HttpClient) {}
+
+  loadVideos(): Observable<Video[]> {
+    return this.http.get<Video[]>(`${environment.baseUrl}/videos`);
+  }
+}
