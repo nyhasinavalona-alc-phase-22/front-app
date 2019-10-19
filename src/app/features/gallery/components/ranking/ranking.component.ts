7b3ac35079541761460713ae2beef760275face7
Ny Hasinavalona Randriantsarafara <randriantsarafaranyhasinavalon@gmail.com>
2019-10-19 19:10:50 +0300

    [GALLERY] Move ranking to an independant component #master

--- src/app/features/gallery/components/ranking/ranking.component.ts
+++ src/app/features/gallery/components/ranking/ranking.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-ranking',
   templateUrl: './ranking.component.html',
-  styleUrls: ['./ranking.component.scss']
+  styleUrls: ['./ranking.component.scss'],
 })
-export class RankingComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+export class RankingComponent {
+  active: number[];
+  inactive: number[];
+  @Input() set stars(value: number) {
+    this.active = new Array(value).fill(1);
+    this.inactive = new Array(5 - value).fill(1);
   }
-
+  @Input() views: number;
 }
