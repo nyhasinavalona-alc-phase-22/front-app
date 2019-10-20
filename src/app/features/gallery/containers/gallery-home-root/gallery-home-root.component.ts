164f5bd047788fc26a8b519e8d8353847c7dcd7e
Ny Hasinavalona Randriantsarafara <randriantsarafaranyhasinavalon@gmail.com>
2019-10-20 15:17:12 +0300

    [GALLERY] Add list's logic #master

--- src/app/features/gallery/containers/gallery-home-root/gallery-home-root.component.ts
+++ src/app/features/gallery/containers/gallery-home-root/gallery-home-root.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { select, Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { State } from 'src/app/core/store/reducers/app.reducers';
+import { getVideos } from '../../store/selectors/video.selectors';
+import { Video } from '../../types/video.interface';
 
 @Component({
   selector: 'app-gallery-home-root',
   templateUrl: './gallery-home-root.component.html',
-  styleUrls: ['./gallery-home-root.component.scss']
+  styleUrls: ['./gallery-home-root.component.scss'],
 })
 export class GalleryHomeRootComponent implements OnInit {
+  videos$: Observable<Video[]>;
 
-  constructor() { }
+  constructor(private store: Store<State>) {}
 
   ngOnInit() {
+    this.videos$ = this.store.pipe(select(getVideos));
   }
-
 }
