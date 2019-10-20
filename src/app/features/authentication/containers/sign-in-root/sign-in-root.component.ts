65b4db1d0f861d35778cd575a9ee21b1599fea0b
Ny Hasinavalona Randriantsarafara <randriantsarafaranyhasinavalon@gmail.com>
2019-10-20 15:15:21 +0300

    [GALLERY] Change  herited state #master

--- src/app/features/authentication/containers/sign-in-root/sign-in-root.component.ts
+++ src/app/features/authentication/containers/sign-in-root/sign-in-root.component.ts
@@ -1,41 +1,40 @@
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { State } from 'src/app/core/store/reducers/app.reducers';
 import { QuestionBase } from '../../../../shared/types/question-base.class';
 import { TextBoxQuestion } from '../../../../shared/types/text-box-question.class';
-import { Store } from '@ngrx/store';
-import { AuthenticationState } from '../../store/reducers/authentication.reducers';
 import { signIn } from '../../store/actions/authentication.actions';
 
 @Component({
-    selector: 'app-sign-in-root',
-    templateUrl: './sign-in-root.component.html',
-    styleUrls: ['./sign-in-root.component.scss']
+  selector: 'app-sign-in-root',
+  templateUrl: './sign-in-root.component.html',
+  styleUrls: ['./sign-in-root.component.scss'],
 })
 export class SignInRootComponent implements OnInit {
-    questions: QuestionBase<any>[];
+  questions: QuestionBase<any>[];
 
-    constructor(private authenticationStore: Store<AuthenticationState>) {
-    }
+  constructor(private store: Store<State>) {}
 
-    ngOnInit() {
-        this.questions = [
-            new TextBoxQuestion({
-                type: 'text',
-                key: 'userName',
-                label: 'User name',
-                required: true,
-                order: 1
-            }),
-            new TextBoxQuestion({
-                type: 'password',
-                key: 'password',
-                label: 'Password',
-                required: true,
-                order: 2
-            })
-        ];
-    }
+  ngOnInit() {
+    this.questions = [
+      new TextBoxQuestion({
+        type: 'text',
+        key: 'userName',
+        label: 'User name',
+        required: true,
+        order: 1,
+      }),
+      new TextBoxQuestion({
+        type: 'password',
+        key: 'password',
+        label: 'Password',
+        required: true,
+        order: 2,
+      }),
+    ];
+  }
 
-    onSave(payload: { userName: string, password: string }) {
-        this.authenticationStore.dispatch(signIn(payload));
-    }
+  onSave(payload: { userName: string; password: string }) {
+    this.store.dispatch(signIn(payload));
+  }
 }
