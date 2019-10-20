82daa4589168135beb0cdd488e8b49104dc065af
Ny Hasinavalona Randriantsarafara <randriantsarafaranyhasinavalon@gmail.com>
2019-10-20 15:15:45 +0300

    [AUTHENTICATION] ident file #master

--- src/app/features/authentication/store/selectors/authentication.selectors.ts
+++ src/app/features/authentication/store/selectors/authentication.selectors.ts
@@ -1,13 +1,31 @@
 import { createFeatureSelector, createSelector } from '@ngrx/store';
-import { AuthenticationState, authenticationStateKey } from '../reducers/authentication.reducers';
 import { State } from '../../../../core/store/reducers/app.reducers';
+import {
+  AuthenticationState,
+  authenticationStateKey,
+} from '../reducers/authentication.reducers';
 
-export const getAuthenticationState = createFeatureSelector<State, AuthenticationState>(authenticationStateKey);
+export const getAuthenticationState = createFeatureSelector<
+  State,
+  AuthenticationState
+>(authenticationStateKey);
 
-export const getSignInError = createSelector(getAuthenticationState, (state: AuthenticationState) => state.signInError);
+export const getSignInError = createSelector(
+  getAuthenticationState,
+  (state: AuthenticationState) => state.signInError,
+);
 
-export const getSigning = createSelector(getAuthenticationState, (state: AuthenticationState) => state.signing);
+export const getSigning = createSelector(
+  getAuthenticationState,
+  (state: AuthenticationState) => state.signing,
+);
 
-export const getSigned = createSelector(getAuthenticationState, (state: AuthenticationState) => state.signed);
+export const getSigned = createSelector(
+  getAuthenticationState,
+  (state: AuthenticationState) => state.signed,
+);
 
-export const getCurrentUser = createSelector(getAuthenticationState, (state: AuthenticationState) => state.currentUser);
+export const getCurrentUser = createSelector(
+  getAuthenticationState,
+  (state: AuthenticationState) => state.currentUser,
+);
