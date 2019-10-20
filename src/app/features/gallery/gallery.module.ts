18d6d32c1fdbef82843121be88bc515e25e88444
Ny Hasinavalona Randriantsarafara <randriantsarafaranyhasinavalon@gmail.com>
2019-10-20 13:09:32 +0300

    [GALLERY] Register video effects and service #master

--- src/app/features/gallery/gallery.module.ts
+++ src/app/features/gallery/gallery.module.ts
@@ -1,20 +1,31 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-import { GalleryRoutingModule } from './gallery-routing.module';
-import { GalleryHomeRootComponent } from './containers/gallery-home-root/gallery-home-root.component';
-import { VideoListComponent } from './components/video-list/video-list.component';
-import { VideoCardComponent } from './components/video-card/video-card.component';
-import { VideoDetailsRootComponent } from './containers/video-details-root/video-details-root.component';
-import { VideoDetailComponent } from './components/video-detail/video-detail.component';
+import { NgModule } from '@angular/core';
+import { EffectsModule } from '@ngrx/effects';
 import { RankingComponent } from './components/ranking/ranking.component';
-
+import { VideoCardComponent } from './components/video-card/video-card.component';
+import { VideoDetailComponent } from './components/video-detail/video-detail.component';
+import { VideoListComponent } from './components/video-list/video-list.component';
+import { GalleryHomeRootComponent } from './containers/gallery-home-root/gallery-home-root.component';
+import { VideoDetailsRootComponent } from './containers/video-details-root/video-details-root.component';
+import { GalleryRoutingModule } from './gallery-routing.module';
+import { VideoService } from './services/video.service';
+import { VideoRouterEffects } from './store/effects/video-router.effects';
+import { VideoEffects } from './store/effects/video.effects';
 
 @NgModule({
-  declarations: [GalleryHomeRootComponent, VideoListComponent, VideoCardComponent, VideoDetailsRootComponent, VideoDetailComponent, RankingComponent],
+  declarations: [
+    GalleryHomeRootComponent,
+    VideoListComponent,
+    VideoCardComponent,
+    VideoDetailsRootComponent,
+    VideoDetailComponent,
+    RankingComponent,
+  ],
   imports: [
     CommonModule,
-    GalleryRoutingModule
-  ]
+    GalleryRoutingModule,
+    EffectsModule.forFeature([VideoEffects, VideoRouterEffects]),
+  ],
+  providers: [VideoService],
 })
-export class GalleryModule { }
+export class GalleryModule {}
