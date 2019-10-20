391d4ead2e1ab0dabdb42083f0483319e74d3c47
Ny Hasinavalona Randriantsarafara <randriantsarafaranyhasinavalon@gmail.com>
2019-10-20 16:43:15 +0300

    [GALLERY] Add video details component's logic #master

--- src/app/features/gallery/components/video-detail/video-detail.component.ts
+++ src/app/features/gallery/components/video-detail/video-detail.component.ts
@@ -1,15 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Video } from '../../types/video.interface';
 
 @Component({
   selector: 'app-video-detail',
   templateUrl: './video-detail.component.html',
-  styleUrls: ['./video-detail.component.scss']
+  styleUrls: ['./video-detail.component.scss'],
 })
-export class VideoDetailComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+export class VideoDetailComponent {
+  @Input() video: Video;
 }
--- src/app/features/gallery/containers/video-details-root/video-details-root.component.ts
+++ src/app/features/gallery/containers/video-details-root/video-details-root.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { select, Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { State } from 'src/app/core/store/reducers/app.reducers';
+import { getVideo } from '../../store/selectors/video.selectors';
+import { Video } from '../../types/video.interface';
 
 @Component({
   selector: 'app-video-details-root',
   templateUrl: './video-details-root.component.html',
-  styleUrls: ['./video-details-root.component.scss']
+  styleUrls: ['./video-details-root.component.scss'],
 })
 export class VideoDetailsRootComponent implements OnInit {
+  video$: Observable<Video>;
 
-  constructor() { }
+  constructor(private store: Store<State>) {}
 
   ngOnInit() {
+    this.video$ = this.store.pipe(select(getVideo));
   }
-
 }
