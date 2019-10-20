3ea233d3a77158524e11a789962db9af4f3efbd0
Ny Hasinavalona Randriantsarafara <randriantsarafaranyhasinavalon@gmail.com>
2019-10-20 15:16:45 +0300

    [GALLERY] Add component's logic #master

--- src/app/features/gallery/components/video-card/video-card.component.ts
+++ src/app/features/gallery/components/video-card/video-card.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Video } from '../../types/video.interface';
 
 @Component({
   selector: 'app-video-card',
   templateUrl: './video-card.component.html',
-  styleUrls: ['./video-card.component.scss']
+  styleUrls: ['./video-card.component.scss'],
 })
 export class VideoCardComponent implements OnInit {
+  @Input() video: Video;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
--- src/app/features/gallery/components/video-list/video-list.component.ts
+++ src/app/features/gallery/components/video-list/video-list.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Video } from '../../types/video.interface';
 
 @Component({
   selector: 'app-video-list',
   templateUrl: './video-list.component.html',
-  styleUrls: ['./video-list.component.scss']
+  styleUrls: ['./video-list.component.scss'],
 })
 export class VideoListComponent implements OnInit {
+  @Input() videos: Observable<Video[]>;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
