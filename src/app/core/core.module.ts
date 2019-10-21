8ad6e7ecb968b04203e5a44b03c68a08ba692289
Ny Hasinavalona Randriantsarafara <randriantsarafaranyhasinavalon@gmail.com>
2019-10-21 09:36:20 +0300

    [CORE] Import forms module #master

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,46 +1,48 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { AuthenticationModule } from '../features/authentication/authentication.module';
-import { StoreModule } from '@ngrx/store';
-import { metaReducers, reducers } from './store/reducers/app.reducers';
-import { StoreDevtoolsModule } from '@ngrx/store-devtools';
-import { environment } from '../../environments/environment';
-import { StoreRouterConnectingModule } from '@ngrx/router-store';
 import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreRouterConnectingModule } from '@ngrx/router-store';
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
-import { InMemoryDBService } from './services/in-memory-db.service';
-import { HomeRootComponent } from './containers/home-root/home-root.component';
+import { environment } from '../../environments/environment';
+import { AuthenticationModule } from '../features/authentication/authentication.module';
+import { AuthenticationEffects } from '../features/authentication/store/effects/authentication.effects';
 import { HeaderComponent } from './components/header/header.component';
 import { SidenavComponent } from './components/sidenav/sidenav.component';
+import { HomeRootComponent } from './containers/home-root/home-root.component';
 import { CoreRoutingModule } from './core-routing.module';
-import { EffectsModule } from '@ngrx/effects';
+import { InMemoryDBService } from './services/in-memory-db.service';
 import { RouterEffects } from './store/effects/router.effects';
-import { AuthenticationEffects } from '../features/authentication/store/effects/authentication.effects';
+import { metaReducers, reducers } from './store/reducers/app.reducers';
 
 @NgModule({
-    declarations: [
-        HomeRootComponent,
-        HeaderComponent,
-        SidenavComponent
-    ],
-    imports: [
-        CommonModule,
-        CoreRoutingModule,
-        AuthenticationModule,
-        HttpClientModule,
-        environment.production ? [] : HttpClientInMemoryWebApiModule.forRoot(InMemoryDBService),
-        StoreModule.forRoot(reducers, {
-            metaReducers,
-            runtimeChecks: {
-                strictStateImmutability: true,
-                strictActionImmutability: true
-            }
-        }),
-        EffectsModule.forRoot([RouterEffects, AuthenticationEffects]),
-        StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production }),
-        StoreRouterConnectingModule.forRoot()
-    ],
-    providers: []
+  declarations: [HomeRootComponent, HeaderComponent, SidenavComponent],
+  imports: [
+    CommonModule,
+    CoreRoutingModule,
+    AuthenticationModule,
+    HttpClientModule,
+    environment.production
+      ? []
+      : HttpClientInMemoryWebApiModule.forRoot(InMemoryDBService),
+    StoreModule.forRoot(reducers, {
+      metaReducers,
+      runtimeChecks: {
+        strictStateImmutability: true,
+        strictActionImmutability: true,
+      },
+    }),
+    EffectsModule.forRoot([RouterEffects, AuthenticationEffects]),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25,
+      logOnly: environment.production,
+    }),
+    StoreRouterConnectingModule.forRoot(),
+    FormsModule,
+  ],
+  providers: [],
 })
-export class CoreModule {
-}
+export class CoreModule {}
